722ecc01d088300cdf9007653eb373aadcbbfe19
Rick Gaiser <rgaiser@gmail.com>
2016-04-22 09:36:49 +0200

    Run test producer and consumer in two separate threads.

--- testcommon.cpp
+++ testcommon.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <thread>
 
 #include "testcommon.h"
 
 
+static bool bError;
+
+
 //---------------------------------------------------------------------------
-void print_data_size(unsigned int size)
+void
+print_data_size(unsigned int size)
 {
 	/*if(size > (5*1024*1024*1024))
 		std::cout<<(size/(1024*1024*1024))<<"GiB";
@@ -18,22 +23,48 @@ void print_data_size(unsigned int size)
 
 //---------------------------------------------------------------------------
 void
+thr_produce(struct testproducer * pprod)
+{
+	std::cout<<"producer running"<<std::endl;
+
+	while ((testproducer_done(pprod) == 0) && (bError == false)) {
+		testproducer_produce(pprod);
+	}
+
+	std::cout<<"producer stopping"<<std::endl;
+}
+
+//---------------------------------------------------------------------------
+void
+thr_consume(struct testconsumer * pcons)
+{
+	std::cout<<"consumer running"<<std::endl;
+
+	while ((testconsumer_done(pcons) == 0) && (bError == false)) {
+		testconsumer_consume(pcons);
+		bError = testconsumer_error(pcons) != 0;
+	}
+
+	std::cout<<"consumer stopping"<<std::endl;
+}
+
+//---------------------------------------------------------------------------
+void
 run_test(struct testproducer * pprod, struct testconsumer * pcons)
 {
-	while (((testproducer_done (pprod) == 0) || (testconsumer_done (pcons) == 0)) &&
-		(testconsumer_error(pcons) == 0))
-	{
-		// Fill the fifo to the IOP
-		testproducer_produce(pprod);
-		// Empty the fifo from the IOP
-		testconsumer_consume(pcons);
-	}
+	bError = false;
+
+	std::thread tProd(thr_produce, pprod);
+	std::thread tCons(thr_consume, pcons);
+
+	tProd.join();
+	tCons.join();
 
 	std::cout<<"Done, produced: ";
 	print_data_size(pprod->actual32*4);
 	std::cout<<    ", consumed: ";
 	print_data_size(pcons->actual32*4);
-	if (testconsumer_error(pcons) != 0)
-		std::cout<<", ERROR!";
+	if (bError == true)
+		std::cout<<", ERROR@nr"<<pcons->actual32;
 	std::cout<<std::endl;
 }
